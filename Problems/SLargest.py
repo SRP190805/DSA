1430f94ffe82ac6f6d191d7fb1ec3248a0cfc8fd
SRP190805 <142969170+SRP190805@users.noreply.github.com>
2024-07-09 10:17:21 -0400

    Create SLargest.py
    !!

---
+++ Problems/SLargest.py
@@ -0,0 +1,16 @@
+def SecLargest(nums,size=0):
+    if size == 0:
+        size = len(nums)
+    Largest = nums[0]
+    SecLargest = -1    
+    for i in range(size):
+        if nums[i] > Largest:
+            SecLargest = Largest
+            Largest = nums[i]
+    for i in range(size):
+        if (nums[i] > SecLargest and nums[i] != Largest):
+            SecLargest = nums[i]
+    print(SecLargest)        
+    
+arr = [int(x) for x in (input("Enter elements:").split(" "))]
+SecLargest(arr)
